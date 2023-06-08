ef08621c14aaf4c05668c0d545b1a79661f606c8
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-08 16:49:53 +0100

    Create meals screen

---
+++ lib/screens/meals.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  Widget buildMealsList(BuildContext ctx, int index) {
+    return Text(meals[index].title);
+  }
+
+  @override
+  Widget build(context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisAlignment: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh... nothing here',
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+          Text(
+            'Try selecting a different category',
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: meals.length, itemBuilder: buildMealsList);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
+}
