763d902121d65d04098818175cdfbfa973eb0274
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-08 16:07:55 +0100

    Separating screens and widgets

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(context) {}
+}
