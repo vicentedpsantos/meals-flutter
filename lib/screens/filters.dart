d21990ea43713712f5867f73abbed78fe8c3fc69
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-11 23:34:13 +0100

    Using WillPopScope to return data from widget that will pop

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -31,37 +31,44 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Your filters'),
       ),
-      body: Column(
-        children: [
-          FilterTile(
-            description: 'Only include gluten-free meals',
-            filterName: 'glutten-free',
-            isSet: false,
-            setFilterValue: _setFilterValue,
-            title: 'Glutten-free',
-          ),
-          FilterTile(
-            description: 'Only include lactose-free meals.',
-            filterName: 'lactose-free',
-            isSet: false,
-            setFilterValue: _setFilterValue,
-            title: 'Lactose-free',
-          ),
-          FilterTile(
-            description: 'Only include vegetarian meals.',
-            filterName: 'vegetarian',
-            isSet: false,
-            setFilterValue: _setFilterValue,
-            title: 'Vegetarian',
-          ),
-          FilterTile(
-            description: 'Only include vegan meals.',
-            filterName: 'vegan',
-            isSet: false,
-            setFilterValue: _setFilterValue,
-            title: 'Vegan',
-          ),
-        ],
+      body: WillPopScope(
+        onWillPop: () async {
+          Navigator.of(context).pop(filters);
+
+          return false;
+        },
+        child: Column(
+          children: [
+            FilterTile(
+              description: 'Only include gluten-free meals',
+              filterName: 'glutten-free',
+              isSet: false,
+              setFilterValue: _setFilterValue,
+              title: 'Glutten-free',
+            ),
+            FilterTile(
+              description: 'Only include lactose-free meals.',
+              filterName: 'lactose-free',
+              isSet: false,
+              setFilterValue: _setFilterValue,
+              title: 'Lactose-free',
+            ),
+            FilterTile(
+              description: 'Only include vegetarian meals.',
+              filterName: 'vegetarian',
+              isSet: false,
+              setFilterValue: _setFilterValue,
+              title: 'Vegetarian',
+            ),
+            FilterTile(
+              description: 'Only include vegan meals.',
+              filterName: 'vegan',
+              isSet: false,
+              setFilterValue: _setFilterValue,
+              title: 'Vegan',
+            ),
+          ],
+        ),
       ),
     );
   }
