6e767a0bb5156cc899494d3705b56726afb0c830
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-11 20:21:59 +0100

    Add drawer

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -77,6 +78,7 @@ class _TabsScreenState extends State<TabsScreen> {
           ),
         ],
       ),
+      drawer: MainDrawer(),
     );
   }
 }
